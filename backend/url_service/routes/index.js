69d2b850e7cb445373d4065a91a06be2a48127a5
Shiv Soni <sonishiv309@gmail.com>
2025-11-23 15:17:25 +0530

    completed get url with redirect

--- backend/url_service/routes/index.js
+++ backend/url_service/routes/index.js
@@ -4,10 +4,12 @@ const get_url_routes = require('./get_url_list_routes');
 const get_url_list_routes = require('./get_url_list_routes');
 const edit_url_routes = require('./edit_url_routes');
 const delete_url_routes = require('./delete_url_routes');
+const redirect_url_routes = require('./redirect_url_routes');
 
 url_routes.use('/app', get_url_routes);
 url_routes.use('/app', edit_url_routes);
 url_routes.use('/app', get_url_list_routes);
-url_routes.use('/app', delete_url_routes)
+url_routes.use('/app', delete_url_routes);
+url_routes.use('/app', redirect_url_routes);
 
 module.exports = url_routes;
