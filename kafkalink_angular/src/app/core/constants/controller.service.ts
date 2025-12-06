0fd199642ac1963380375e7bcb5a526eaf572408
Shiv Soni <sonishiv309@gmail.com>
2025-12-06 19:12:58 +0530

    installed prime ng

--- kafkalink_angular/src/app/core/constants/controller.service.ts
+++ kafkalink_angular/src/app/core/constants/controller.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from "@angular/core";
 
 @Injectable({
-    providedIn : 'root'
+    providedIn: 'root'
 })
 
 export class Controllers {
 
-    public readonly login:string =  'http://8001/login';
-    public readonly signup: string = 'http://8002/signup'
+    public readonly authentication = {
+        login: 'http://8001/login',
+        signup: 'http://8002/signup'
+    };
 }
