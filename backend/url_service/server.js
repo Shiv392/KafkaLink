ac06380ee445d3b10bc398dbec8fa74cb2a0c11c
Shiv Soni <sonishiv309@gmail.com>
2025-12-14 16:50:57 +0530

    installed websocket for realtime communication

--- backend/url_service/server.js
+++ backend/url_service/server.js
@@ -1,5 +1,11 @@
+const http = require('http');
 const app = require('./config/server_config');
+const {init_websocket} = require('./config/websocket_config');
 
-app.listen(Number(process.env.SERVER_PORT), ()=>{
-    console.log(`server started http://localhost:${process.env.SERVER_PORT}`);
-})
+const server = http.createServer(app);
+
+init_websocket(server);
+
+server.listen(Number(process.env.SERVER_PORT), () => {
+    console.log(`🚀 Server started at http://localhost:${process.env.SERVER_PORT}`);
+});
