67d2da7a2a90316fd8b7436f24f6248fa3b74e33
Shiv Soni <sonishiv309@gmail.com>
2025-12-22 23:40:27 +0530

    completed signup api

---
+++ kafkalink_angular/src/app/pages/authentication/models/signup.model.ts
@@ -0,0 +1,3 @@
+export interface signup_api_model{
+    message : string
+}
--- kafkalink_angular/src/app/pages/authentication/services/signup.service.ts
+++ kafkalink_angular/src/app/pages/authentication/services/signup.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable } from "@angular/core";
 import { Controllers } from "../../../core/constants/controller.service";
 import { environment } from "../../../../environments/environment";
 import { catchError, Observable, throwError } from "rxjs";
+import { signup_api_model } from "../models/signup.model";
 
 @Injectable({
     providedIn : 'root'
@@ -12,7 +13,7 @@ export class SignupService{
     public http = inject(HttpClient);
     public controller = inject(Controllers);
 
-    public signup(apibody : {name : string, email : string, password : string}) : Observable<{success : boolean, message : string}>{
+    public signup(apibody : {name : string, email : string, password : string}) : Observable<signup_api_model>{
     const url = environment.api.auth+this.controller.authentication.signup;
     return this.http.post<any>(url, apibody).pipe(
         catchError((error : HttpErrorResponse)=>{
